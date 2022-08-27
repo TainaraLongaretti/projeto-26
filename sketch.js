1344acb8a7992e33ee5ad07ca0c4aac3f79304ae
Bia <82560900+BiatrizR@users.noreply.github.com>
2022-08-26 21:57:58 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,12 @@ const Body = Matter.Body;
 
 var retangulo1, retangulo2, retangulo3;
 
+
+
 var angle=60;
+//definir uma variavel de angulo para casa objeto, podendo conter todos o mesmo valor ou não
+var angle2=60;
+var angle3=60;
 
 
 function preload()
@@ -21,9 +26,10 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	//Crie os Corpos aqui.
+	//para o projeto funcionar corretamente apenas ficou faltando inserir  atributo isStatic, para os objetos não caírem.
 	
-	var retangulo_option = { restitution:0.4, friction:0.02 }
+	var retangulo_option = { isStatic: true, 
+				restitution:0.4, friction:0.02 }
 
 	retangulo1=Bodies.rectangle(250, 200, 150, 20, retangulo_option);
 	retangulo2=Bodies.rectangle(250, 200, 150, 20, retangulo_option);
@@ -43,7 +49,6 @@ function draw() {
   rectMode(CENTER);
   background("Aquamarine");
 
-  Matter.Body.rotate(retangulo1,angle);
   push(); 
   translate(retangulo1.position.x,retangulo1.position.y); 
   rotate(angle);
@@ -52,8 +57,15 @@ function draw() {
   rect(0, 0, 150, 20);
   pop(); 
   angle +=0.2;
+	
+//criar uma nova configuração para cada objeto(2 e 3), sempre entre push(); e pop(); pois cada objeto rotacionara em direções e velocidade diferentes	
+  push(); 
+  translate(retangulo2.position.x,retangulo2.position.y); 
+  rotate(angle2);
+  rect(0, 0, 150, 20);
+  pop(); 
+  angle2 -=0.5;
 
-  rect(retangulo2.position.x, retangulo2.position.y, 150, 20);
   rect(retangulo3.position.x, retangulo3.position.y, 150, 20);
 
   
